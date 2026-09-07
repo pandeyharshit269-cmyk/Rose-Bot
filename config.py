56f23e25096feba55247001f1a1b4b1483ef52fd
pandeyharshit269-cmyk <pandeyharshit269@gmail.com>
2026-09-07 19:42:45 +0530

    Change database URI from MongoDB to PostgreSQL

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Development(Config):
     OWNER_ID = 8700952672  # my telegram ID
     OWNER_USERNAME = "NAKS4PANDIT"  # my telegram username
     API_KEY = "8010952431:AAFRiWPMgyCJ5yZjruC0ngwG0rgJewxW45c"  # my api key, as provided by the botfather
-    SQLALCHEMY_DATABASE_URI = 'mongodb+srv://devilharshit02:devilharshit02@cluster0.9efk59a.mongodb.net/?appName=Cluster0'  # sample db credentials
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:jFCobtpmTMJHmXRppoYYguguGghuitsM@postgres.railway.internal:5432/railway'  # sample db credentials
     MESSAGE_DUMP = '-1003894294224' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
     SUDO_USERS = [6154383311, 5385377266]  # List of id's for users which have sudo access to the bot.
